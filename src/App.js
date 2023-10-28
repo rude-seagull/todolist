4661cc1425016e0606201c83913c54f0dbee52a8
rudeseagull <jun.capellen@gmail.com>
2023-10-28 16:23:11 +0200

    Add Todolist

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Todo from "./components/Todo";
+import Todolist from "./components/Todolist";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import { useState } from "react";
@@ -12,66 +12,23 @@ const FILTER_MAP = {
 const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App({ initialTasks }) {
-  const [tasks, setTasks] = useState(initialTasks);
   const [filter, setFilter] = useState("All");
+  const [tasks, setTasks] = useState(initialTasks);
 
   function addTask(name) {
     setTasks([...tasks, { id: "todo-" + tasks.length, name: name, completed: false }])
   }
 
-  function deleteTask(id) {
-    const remainingTasks = tasks.filter(task => id !== task.id);
-    setTasks(remainingTasks);
-  }
-
-  function editTask(id, newName) {
-    const editedTasks = tasks.map(task => {
-      if (id === task.id) {
-        return { ...task, name: newName }
-      }
-      return task;
-    });
-    setTasks(editedTasks);
-  }
-
-  function toggleCompleteStatus(id) {
-    const updatedTasks = tasks.map(task => {
-      if (id === task.id) {
-        return { ...task, completed: !task.completed }
-      }
-      return task;
-    });
-    setTasks(updatedTasks);
-  }
-
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
+      <h1>Ze Todo</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         {FILTER_NAMES.map((name) => (
           <FilterButton key={name} name={name} isActive={name === filter} setFilter={setFilter} />
         ))}
       </div>
-      <h2 id="list-heading">{tasks.length} {tasks.length !== 1 ? "tasks" : "task"} remaining</h2>
-      <ul
-        role="list"
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading">
-        {tasks
-          .filter(FILTER_MAP[filter])
-          .map((task) => (
-            <Todo
-              id={task.id}
-              name={task.name}
-              completed={task.completed}
-              key={task.id}
-              toggleCompleted={toggleCompleteStatus}
-              deleteTask={deleteTask}
-              editTask={editTask}
-            />
-          ))}
-      </ul>
+      <Todolist tasks={tasks} setTasks={setTasks} filterMap={FILTER_MAP} filter={filter} />
     </div>
   );
 }
---
+++ src/components/Todolist.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Todo from "./Todo";
+
+function Todolist({ tasks, setTasks, filterMap, filter }) {
+    function deleteTask(id) {
+        const remainingTasks = tasks.filter(task => id !== task.id);
+        setTasks(remainingTasks);
+    }
+
+    function editTask(id, newName) {
+        const editedTasks = tasks.map(task => {
+            if (id === task.id) {
+                return { ...task, name: newName }
+            }
+            return task;
+        });
+        setTasks(editedTasks);
+    }
+
+    function toggleCompleteStatus(id) {
+        const updatedTasks = tasks.map(task => {
+            if (id === task.id) {
+                return { ...task, completed: !task.completed }
+            }
+            return task;
+        });
+        setTasks(updatedTasks);
+    }
+
+    return (
+        <React.Fragment>
+            <h2 id="list-heading">{tasks.length} {tasks.length !== 1 ? "tasks" : "task"} remaining</h2>
+            <ul
+                role="list"
+                className="todo-list stack-large stack-exception"
+                aria-labelledby="list-heading">
+                {tasks
+                    .filter(filterMap[filter])
+                    .map((task) => (
+                        <Todo
+                            id={task.id}
+                            name={task.name}
+                            completed={task.completed}
+                            key={task.id}
+                            toggleCompleted={toggleCompleteStatus}
+                            deleteTask={deleteTask}
+                            editTask={editTask}
+                        />
+                    ))}
+            </ul>
+        </React.Fragment>
+    )
+}
+
+export default Todolist;
